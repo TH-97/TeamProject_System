0c2ebfb49e7b385dac8c7c6f9bc640837e1cf1f4
TH_97 <hood3392@gmail.com>
2024-03-13 19:49:22 +0900

    base 생성

---
+++ Project_domitory/src/main/java/com/project/domitory/ProjectDomitoryApplication.java
@@ -0,0 +1,13 @@
+package com.project.domitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDomitoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDomitoryApplication.class, args);
+    }
+
+}
