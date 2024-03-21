b00feefa4db2e8e7ce0321430e88a8fc7a366e88
TH_97 <hood3392@gmail.com>
2024-03-21 10:35:08 +0900

    건물 설계도(?)

--- Project_domitory/src/main/resources/static/js/AdminPageJs/adminMainPage.js
+++ Project_domitory/src/main/resources/static/js/AdminPageJs/adminMainPage.js
@@ -14,7 +14,23 @@ $("#tree").treeview({
   penaltyDetails.addEventListener("click", ()=>{
   var main = document.querySelector("#main");
   main.src = "adminHtml/penaltyDetails.html"
+  });
 
-});
+  var Afloor = document.querySelector("#Afloor");
+var Afloor2 = document.querySelector("#Afloor2")
+var Afloor3 = document.querySelector("#Afloor3")
+
+  Afloor.addEventListener("click", ()=>{
+  var main = document.querySelector("#main");
+  main.src = "adminHtml/A1F.html"
+  });
+  Afloor2.addEventListener("click", ()=>{
+  var main = document.querySelector("#main");
+  main.src = "adminHtml/A2F.html"
+  });Afloor3.addEventListener("click", ()=>{
+  var main = document.querySelector("#main");
+  main.src = "adminHtml/A3F.html"
+  });
+
 
 
